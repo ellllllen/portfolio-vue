e24c65d97d03e2faa1d0046992d266f84981e68d
Ellen <ellenmerchant@gmail.com>
2019-04-22 16:31:56 +0100

    Fix build error

---
+++ src/components/articles/Article.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 class="main-title">{{ article.title }}</h2>
+    <div class="mb-3">
+      <i>
+        Created:
+        {{ this.moment(article.created_at).format("Y-MM-DD") }}
+      </i>
+      <br />
+      <i>
+        Last Updated:
+        {{ this.moment(article.updated_at).format("Y-MM-DD") }}
+      </i>
+    </div>
+    <div v-html="article.section"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "article",
+  props: ["article"]
+};
+</script>
