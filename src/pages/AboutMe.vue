856e911b1aaaf4fdf6f812256e00691060967dad
Ellen Merchant <ellenmerchant@gmail.com>
2020-05-02 10:09:19 +0100

    Update data and styling tweaks

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="about-me">
     <h2 class="main-title">About Me</h2>
-    Hi, I'm Ellen and I currently live in Cirencester, Gloucestershire working
+    Hi, I'm Ellen and I currently live in Bristol working
     as a full time Developer. I graduated from Bournemouth University in 2010,
-    gaining a 2:1 BSc (Hons) in Computing. I specialise in PHP, MySQL, HTML, CSS
+    gaining a 2:1 BSc (Hons) in Computing. I specialise in PHP, SQL, HTML, CSS
     and JavaScript and have experience in developing using the
     <a href="https://laravel.com">Laravel</a>
     framework. I have been employed as a full time Developer for over
     {{ moment().diff(moment([2010, 5, 1]), "years") }} years.
     <br />
-    <br />My hobbies include walking, photography, film and TV, music and Harry
+    <br />My hobbies include walking, gaming, film and TV, music and Harry
     Potter.
     <br />
     <br />Here is my GitHub for your perusal:
